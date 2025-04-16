bd642cd17e164e3d42fb9458e4d1a76a55169362
lenaaa0 <2487478719@qq.com>
2025-04-17 00:06:04 +0800

    base64-ok

--- src/api/base64.js
+++ src/api/base64.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:8080/api/base64';
 
-export const base64Encode = (data) => {
+export const encode = (data) => {
     return axios.post(`${API_URL}/encode`, {
         data
     });
 };
 
-export const base64Decode = (data) => {
+export const decode = (data) => {
     return axios.post(`${API_URL}/decode`, {
         data
     });
